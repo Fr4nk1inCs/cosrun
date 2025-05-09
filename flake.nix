53bda9bf5792afe9dd2fd52266a5e555afa0ac02
Fr4nk1in <fushen@mail.ustc.edu.cn>
2025-05-09 11:50:40 +0800

    feat: flake devshell and direnv

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Devshells for maturin development";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            (
+              python311.withPackages
+              (ps:
+                with ps; [
+                  pip
+                  virtualenv
+                  maturin
+                ])
+            )
+            cargo
+            rustc
+            rustfmt
+            pre-commit
+            rustPackages.clippy
+          ];
+        };
+      }
+    );
+}
